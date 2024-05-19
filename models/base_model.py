5789a6cd480d9ff8b30c887aca1af3ccf9cdd009
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-05-20 01:50:30 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -7,17 +7,18 @@ class BaseModel:
     
     def __init__(self, *args, **kwargs):
         """Initialize a new instance of BaseModel."""
-        if kwargs:
-            for keyy, value in kwargs.items():
+         if kwargs:
+            for key, value in kwargs.items():
                 if key != '__class__':
-                    if key in ['created_at', 'updated_at']:
-                        setattr(self, key, datetime.steptime(value, "%Y-%m-%dT%H:%S.%f"))
-                    else:
-                        setattr(self, key, value)
-                    else:
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+                    setattr(self, key, value)
+            if 'created_at' in kwargs:
+                self.created_at = datetime.fromisoformat(kwargs['created_at'])
+            if 'updated_at' in kwargs:
+                self.updated_at = datetime.fromisoformat(kwargs['updated_at'])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Return a string representation of the instance."""
@@ -26,6 +27,7 @@ class BaseModel:
     def save(self):
         """Update the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Return a dictionary representation of the instance."""
