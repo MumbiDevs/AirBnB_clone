b61ab024a12e61bb55552ac8c1bb5374087cddfd
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-05-20 02:41:46 +0300

    Update console.py

--- console.py
+++ console.py
@@ -9,11 +9,11 @@ class HBNBCommand(cmd.Cmd):
     """Command interpreter class."""
     prompt = '(hbnb) '
 
-    def do_quit(self, line):
+    def do_quit(self, arg):
         """Quit command to exit the program."""
         return True
 
-    def do_EOF(self, line):
+    def do_EOF(self, arg):
         """EOF command to exit the program."""
         print()
         return True
