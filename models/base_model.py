9be2c3c4261d4e066952735c4cf2f6d30dbfab2b
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-05-21 14:55:46 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -12,24 +12,24 @@ class BaseModel:
     """Defines all common attributes/methods for other classes."""
     
     def __init__(self, *args, **kwargs):
-    """Initialize a new instance of BaseModel."""
-    
-    DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
-    if kwargs:
-        for key in kwargs:
-            if key == "created_at":
-                self.__dict__["created_at"] = datetime.strptime(
-                    kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-            elif key == "updated_at":
-                self.__dict__["updated_at"] = datetime.strptime(
-                    kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-            else:
-                self.__dict__[key] = kwargs[key]
-    else:
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        storage.new(self)
+        """Initialize a new instance of BaseModel."""
+        
+        DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
